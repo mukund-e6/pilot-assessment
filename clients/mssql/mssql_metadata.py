1da6de2b79333edde2a3b9fd159099df2b34d3f4
mukund-e6 <a2251@A2251s-MacBook-Pro.local>
2024-11-26 16:00:32 +0530

    <PRE-xx> Mukund added support for mssql server.

---
+++ clients/mssql/mssql_metadata.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import pymssql
+import os
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def run_query_and_save_to_csv(cursor, query, csv_filename, csv_output_dir):
+    try:
+        logger.info(f"Executing query for {csv_filename} metadata")
+        cursor.execute(query)
+        result = cursor.fetchall()
+
+        columns = [desc[0] for desc in cursor.description]
+
+        df = pd.DataFrame(result, columns=columns)
+
+        output_path = os.path.join(csv_output_dir, f'{csv_filename}.csv')
+        df.to_csv(output_path, index=False)
+        logger.info(f"Data written to {csv_filename}")
+    except Exception as e:
+        logger.error(f"Failed to execute query for {csv_filename}: {e}")
+
+
+def extract_metadata():
+    server = os.environ.get('MSSQL_SERVER')
+    port = os.environ.get('MSSQL_PORT')
+    user = os.environ.get('MSSQL_USER')
+    password = os.environ.get('MSSQL_PASSWORD')
+    database = os.environ.get('MSSQL_DATABASE')
+    csv_output_dir = 'mssql-metadata'
+    os.makedirs(csv_output_dir, exist_ok=True)
+    logger.info("Connecting to MSSQL Server...")
+    try:
+        conn = pymssql.connect(
+            server=server,
+            user=user,
+            password=password,
+            database=database,
+            port=port,
+            as_dict=True
+        )
+        cursor = conn.cursor()
+        logger.info("Connected to MSSQL Server")
+        queries = {
+            'schema': """select tbl.table_schema , tbl.table_name,col.column_name, col.data_type, a.rows
+                        FROM information_schema.tables tbl
+                        INNER JOIN information_schema.columns col 
+                            ON col.table_name = tbl.table_name
+                            AND col.table_schema = tbl.table_schema
+                        join 
+                        (SELECT 
+                                t.name AS TableName,
+                                p.rows
+                            FROM sys.tables t
+                            INNER JOIN sys.partitions p ON t.object_id = p.object_id
+                            WHERE p.index_id IN (0, 1)) a
+                            on tbl.table_name=a.TableName
+                            ORDER BY TableName;""",
+            'column_min_max': """DECLARE @sql NVARCHAR(MAX);
+                            SET @sql = N'';
+                            
+                            SELECT @sql += 
+                                'SELECT ''' + t.name + ''' AS TableName, ''' + c.name + ''' AS ColumnName, 
+                                        MIN(' + c.name + ') AS MinValue, 
+                                        MAX(' + c.name + ') AS MaxValue 
+                                 FROM ' + s.name + '.' + t.name + ' UNION ALL '
+                            FROM sys.tables t
+                            JOIN sys.schemas s ON t.schema_id = s.schema_id
+                            JOIN sys.columns c ON t.object_id = c.object_id
+                            JOIN sys.types tp ON c.user_type_id = tp.user_type_id
+                            WHERE tp.name IN ('int', 'bigint', 'smallint', 'decimal', 'numeric', 'float', 'real');
+                            
+                            SET @sql = LEFT(@sql, LEN(@sql) - 10);
+                            
+                            EXEC sp_executesql @sql;""",
+        }
+        for csv_filename, query in queries.items():
+
+            run_query_and_save_to_csv(cursor, query, csv_filename, csv_output_dir)
+        cursor.close()
+        conn.close()
+        logger.info("Connection Closed")
+    except Exception as e:
+        logger.error(f"Error extracting Snowflake metadata: {str(e)}")
---
+++ clients/mssql/mssql_querylogs.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import pymssql
+from datetime import datetime
+import os
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def extract_query_logs():
+    server = os.environ.get('MSSQL_SERVER')
+    port = os.environ.get('MSSQL_PORT')
+    user = os.environ.get('MSSQL_USER')
+    password = os.environ.get('MSSQL_PASSWORD')
+    database = os.environ.get('MSSQL_DATABASE')
+    query_log_start = os.environ.get('QUERY_LOG_START')
+    query_log_end = os.environ.get('QUERY_LOG_END')
+    csv_output_dir = 'mssql-query-logs'
+    os.makedirs(csv_output_dir, exist_ok=True)
+    logger.info("Connecting to MSSQL Server...")
+    try:
+        conn = pymssql.connect(
+            server=server,
+            user=user,
+            password=password,
+            database=database,
+            port=port,
+            as_dict=True
+        )
+        cursor = conn.cursor()
+        logger.info("Connected to MSSQL Server")
+        start_date = datetime.strptime(query_log_start, '%Y-%m-%d')
+        end_date = datetime.strptime(query_log_end, '%Y-%m-%d')
+
+        history_query = f"""
+                        SELECT * FROM sys.dm_exec_query_stats AS qs
+                        CROSS APPLY sys.dm_exec_sql_text(qs.sql_handle) AS st
+                        WHERE qs.creation_time >= '{start_date}'
+                        AND qs.creation_time <= '{end_date}';
+                        """
+        logger.info("Extracting Query logs...")
+        cursor.execute(history_query)
+        rows = cursor.fetchall()
+
+        columns = [column[0] for column in cursor.description]
+
+        df = pd.DataFrame.from_records(rows, columns=columns)
+        if df.empty:
+            logger.info(f"No queries were found between {query_log_start} and {query_log_end}.")
+        else:
+            df['creation_date'] = pd.to_datetime(df['creation_time']).dt.date
+            logger.info(f"Writing query history into csv...")
+            for date, group in df.groupby('creation_date'):
+                csv_filename = f"{csv_output_dir}/query_history_{date}.csv"
+                group.to_csv(csv_filename, index=False)
+                logger.info(f"Data for {date} has been exported to {os.path.basename(csv_filename)}")
+
+            logger.info(f"Query Log Successfully Exported to {csv_output_dir}")
+        cursor.close()
+        conn.close()
+        logger.info("Connection Closed")
+    except Exception as e:
+        logger.error(f"Error extracting MSSQL metadata: {str(e)}")
+
+
